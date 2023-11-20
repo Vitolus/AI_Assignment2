7c24e8e16ebf5e0279f17cd8e717124d20749e90
Vitolus <vito.davide00@gmail.com>
2023-11-20 16:36:42 +0100

    update linear svm

--- models/Classifier.py
+++ models/Classifier.py
@@ -1,8 +1,11 @@
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
 
 
 class Classifier:
     def __init__(self):
-        self.X, self.y = fetch_openml('mnist_784', version=1, return_X_y=True)  # load data
-        self.y = self.y.astype(int)  # convert string to int
-        self.X = self.X / 255.  # normalize data
+        x, y = fetch_openml('mnist_784', version=1, return_X_y=True)  # load data
+        y = y.astype(int)  # convert string to int
+        x = x / 255.  # normalize data
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            x, y, test_size=0.2, random_state=42)  # split data
--- models/Svm.py
+++ models/Svm.py
@@ -1,6 +1,23 @@
 import Classifier
+from sklearn.model_selection import GridSearchCV
+from sklearn import svm
 
 
 class Svm(Classifier):
     def __init__(self):
-        super.__init__(self)
+        super.__init__(self)  # call parent constructor
+        self.hyperparams = {  # hyperparameters to test
+            'C': [1, 10, 100, 1000],  # C is the penalty parameter of the error term
+            'gamma': [0.001, 0.0001],  # gamma is the kernel coefficient
+        }
+        self.model = GridSearchCV(svm.linearSVC(), self.hyperparams, cv=10)  # 10-fold cross validation
+
+    def train(self):  # train the model
+        self.model.fit(self.X_train, self.y_train)  # train the model
+        return self.model.best_params_  # return the best hyperparameters
+
+    def predict(self, x):  # predict the label of x
+        return self.model.predict(x)  # return the predicted label
+
+    def _test(self):  # test the model
+        return self.predict(self.X_test)  # return the predicted labels of the test set
