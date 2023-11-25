29fc881b581b6ccd038c27bd2d0d8d913962bc11
Vitolus <vito.davide00@gmail.com>
2023-11-25 17:31:28 +0100

    add random forest

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 import models.LinearSvm as LSvm
 import models.Svm as Svm
+import models.RandForest as rf
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 
 
 def linear_svm():
-    svm_clf = LSvm.LinearSvm({'C': [0.01, 0.1, 1]})  # create an instance of the Svm class
-    linear_estimator, linear_params = svm_clf.train()  # train the model
+    classifier = LSvm.LinearSvm({'C': [0.01, 0.1, 1]})  # create an instance of the Svm class
+    linear_estimator, linear_params = classifier.train()  # train the model
     print("Best linear params: ", linear_params)  # print the best hyperparameters
-    print(classification_report(svm_clf.y_test, svm_clf.test()))
-    df = svm_clf.get_results()
+    print(classification_report(classifier.y_test, classifier.test()))
+    df = classifier.get_results()
     df.to_excel('linear_svm.xlsx')  # save the results to an excel file
     df.plot()
 
 
 def poly_svm():
-    svm_clf = Svm.Svm({'C': [0.01, 0.1, 1], 'kernel': 'poly', 'degree': 2})
-    poly_estimator, poly_params = svm_clf.train()
+    classifier = Svm.Svm({'C': [0.01, 0.1, 1], 'kernel': ['poly'], 'degree': [2]})
+    poly_estimator, poly_params = classifier.train()
     print("Best polynomail params: ", poly_params)
-    print(classification_report(svm_clf.y_test, svm_clf.test()))
-    df = svm_clf.get_results()
+    print(classification_report(classifier.y_test, classifier.test()))
+    df = classifier.get_results()
     df.to_excel('poly_svm.xlsx')
     df.plot()
 
 
 def rbf_svm():
-    svm_clf = Svm.Svm({'C': [0.01, 0.1, 1], 'kernel': 'rbf'})
-    rbf_estimator, rbf_params = svm_clf.train()
+    classifier = Svm.Svm({'C': [0.01, 0.1, 1], 'kernel': ['rbf']})
+    rbf_estimator, rbf_params = classifier.train()
     print("Best rbf params: ", rbf_params)
-    print(classification_report(svm_clf.y_test, svm_clf.test()))
-    df = svm_clf.get_results()
+    print(classification_report(classifier.y_test, classifier.test()))
+    df = classifier.get_results()
     df.to_excel('rbf_svm.xlsx')
     df.plot()
 
 
+def random_forest():
+    classifier = rf.RandForest({'n_estimators': [10, 100, 1000], 'criterion': ['log_loss', 'entropy'],
+                                'max_features': ['sqrt', 'log2', None], 'bootstrap': [True, False]})
+    rf_estimator, rf_params = classifier.train()
+    print("Best random forest params: ", rf_params)
+    print(classification_report(classifier.y_test, classifier.test()))
+    df = classifier.get_results()
+    df.to_excel('random_forest.xlsx')
+    df.plot()
+
+
 if __name__ == '__main__':
-    linear_svm()
-    poly_svm()
-    rbf_svm()
+    # linear_svm()
+    # poly_svm()
+    # rbf_svm()
+    random_forest()
     plt.show()
---
+++ models/RandForest.py
@@ -0,0 +1,11 @@
+from models.Classifier import Classifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+
+class RandForest(Classifier):
+    def __init__(self, hyperparams):
+        super().__init__()  # call parent constructor
+        # self.X_train, self.X_test, self.y_train, self.y_test self.results are inherited from Classifier
+        self.model = GridSearchCV(RandomForestClassifier(random_state=42), hyperparams, cv=10, n_jobs=-1,
+                                  return_train_score=True, verbose=3)
