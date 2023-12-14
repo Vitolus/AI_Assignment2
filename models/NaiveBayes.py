683bccc27d738e07188d7eee0f23517408e23d79
Vitolus <vito.davide00@gmail.com>
2023-12-14 19:37:30 +0100

    add Naive Bayes

--- models/NaiveBayes.py
+++ models/NaiveBayes.py
@@ -1,18 +1,18 @@
 import cupy as cp
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.metrics import pairwise_distances, accuracy_score
+from scipy.special import gamma
+from sklearn.metrics import accuracy_score
 import time
 
 
-class Knn:
+class NaiveBayes:
 
-    def __init__(self, n_neighbors=5):
+    def __init__(self):
         # Load data
         X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
         y = y.astype(int)  # convert string to int
         X = X / 255.  # normalize data
-        self.n_neighbors = n_neighbors
         # Use a subset of the data (10,000 samples) for both training and testing
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=10000,
                                                                                 random_state=1)
@@ -21,6 +21,8 @@ class Knn:
         self.y_train = cp.array(self.y_train)
         self.X_test = cp.array(self.X_test)
         self.y_test = cp.array(self.y_test)
+        self.params = []
+        self.classes = cp.unique(self.y_train)
 
     def fit(self):
         # Perform 10-fold cross-validation
@@ -31,16 +33,23 @@ class Knn:
             start_time = time.perf_counter()
             X_fold_train, X_fold_val = self.X_train.take(train_index, axis=0), self.X_train.take(val_index, axis=0)
             y_fold_train, y_fold_val = self.y_train.take(train_index), self.y_train.take(val_index)
-            # Compute pairwise distances
-            distances = pairwise_distances(X_fold_val.get(), X_fold_train.get())
-            # Find k-nearest neighbors
-            knn_indices = cp.argpartition(distances, kth=self.n_neighbors, axis=1)[:, :self.n_neighbors]
-            knn_labels = cp.take(y_fold_train, knn_indices)
-            # Make predictions by choosing the most common label among neighbors
-            y_pred_val = []
-            for labels in knn_labels:
-                y_pred_val.append(cp.argmax(cp.bincount(labels)))
-            y_pred_val = cp.array(y_pred_val)
+            self.params = []
+            for i, c in enumerate(self.classes):
+                X_where_c = X_fold_train[cp.where(y_fold_train == c)]
+                E_X = cp.mean(X_where_c, axis=0)
+                Var_X = cp.var(X_where_c, axis=0)
+                K = (E_X * (1 - E_X)) / Var_X - 1
+                alpha = K*E_X
+                beta = K*(1-E_X)
+                self.params.append((alpha, beta))
+            N = X_fold_val.shape[0]
+            C = len(self.classes)
+            P = cp.zeros((N, C))
+            for i in range(C):
+                alpha, beta = self.params[i]
+                P[:, i] = cp.sum(cp.log(gamma(alpha + beta)) - cp.log(gamma(alpha)) - cp.log(gamma(beta)) + (alpha - 1)
+                                 * cp.log(X_fold_val) + (beta - 1) * cp.log(1 - X_fold_val), axis=1)
+            y_pred_val = self.classes[cp.argmax(P, axis=1)]
             # Compute accuracy for this fold
             accuracy_fold = accuracy_score(y_fold_val.get(), y_pred_val.get())
             accuracy_list.append(accuracy_fold)
@@ -53,17 +62,14 @@ class Knn:
 
     def predict(self):
         start_time = time.perf_counter()
-        # Compute pairwise distances
-        distances = pairwise_distances(self.X_test.get(), self.X_train.get())
-        # Find k-nearest neighbors
-        knn_indices = cp.argpartition(distances, kth=self.n_neighbors, axis=1)[:, :self.n_neighbors]
-        knn_labels = cp.take(self.y_train, knn_indices)
-        # Make predictions by choosing the most common label among neighbors
-        y_pred_test = []
-        for labels in knn_labels:
-            y_pred_test.append(cp.argmax(cp.bincount(labels)))
-        y_pred_test = cp.array(y_pred_test)
-        # Compute accuracy on the test set
-        test_accuracy = accuracy_score(self.y_test.get(), y_pred_test.get())
+        N = self.X_test.shape[0]
+        C = len(self.classes)
+        P = cp.zeros((N, C))
+        for i in range(C):
+            alpha, beta = self.params[i]
+            P[:, i] = cp.sum(cp.log(gamma(alpha + beta)) - cp.log(gamma(alpha)) - cp.log(gamma(beta)) + (alpha - 1)
+                             * cp.log(self.X_test) + (beta - 1) * cp.log(1 - self.X_test), axis=1)
+        y_pred_test = self.classes[cp.argmax(P, axis=1)]
+        accuracy = accuracy_score(self.y_test.get(), y_pred_test.get())
         print(f'Test Set time: {time.perf_counter() - start_time} seconds')
-        print(f'Test Set Accuracy: {test_accuracy}')
+        print(f'Test Set Accuracy: {accuracy}')
